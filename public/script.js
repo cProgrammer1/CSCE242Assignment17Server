50d7a6620f5180bc489013e91699dae229515605
cProgrammer1 <chrissy.dobrowolski@gmail.com>
2024-04-07 20:31:54 -0400

    Assignment 17; eleventh commit

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ const getCrafts = async() =>
     try
     {
         // let response = await fetch("http://localhost:3000/api/crafts");
-        let response = await fetch("https://csce242assignment15server.onrender.com/api/crafts");
+        let response = await fetch("https://csce242assignment17server.onrender.com/api/crafts");
         return await response.json();
 
     }
